2d825243c605a906846d72f0047a6390d9241841
Kenneth Christensen <1kennethac@gmail.com>
2019-01-31 10:28:47 -0700

    Initial commit with basic home page.

---
+++ scripts/charts.js
@@ -0,0 +1,90 @@
+let totalProgress = {
+    datasets: [{
+        label: "Total Semester Progress",
+        data: [20, 80],
+        backgroundColor: [
+            successColor,
+            primaryColor
+        ]
+    }],
+    labels: [
+        "Complete",
+        "Incomplete"
+    ]
+};
+
+let englishProgress = {
+    datasets: [{
+        label: "English 316 Progress",
+        data: [45, 55],
+        backgroundColor: [
+            successColor,
+            primaryColor
+        ]
+    }],
+    labels: [
+        "Complete",
+        "Incomplete"
+    ]
+}
+
+let mathProgress = {
+    datasets: [{
+        label: "Math 313 Progress",
+        data: [12, 88],
+        backgroundColor: [
+            successColor,
+            primaryColor
+        ]
+    }],
+    labels: [
+        "Complete",
+        "Incomplete"
+    ]
+}
+
+let weekProgress = {
+    datasets: [{
+        label: "Progress this Week",
+        data: [96, 4],
+        backgroundColor: [
+            successColor,
+            primaryColor
+        ]
+    }],
+    labels: [
+        "Complete",
+        "Incomplete"
+    ]
+}
+
+let dataSets = {
+    'total': totalProgress,
+    'english': englishProgress,
+    'math': mathProgress,
+    'week': weekProgress
+}
+
+function initCharts() {
+    let charts = document.querySelectorAll(".chart");
+    for (var chart of charts) {
+        let dataSet = chart.dataset.chart;
+        initChart(chart.getContext('2d'), dataSets[dataSet]);
+    }
+}
+
+function initChart(context, dataset) {
+    new Chart(context, {
+        type: 'pie',
+        data: dataset,
+        options: {
+            title: {
+                display: true,
+                text: dataset.datasets[0].label,
+                fontSize: 24
+            }
+        }
+    });
+}
+
+window.onload = initCharts;
---
+++ scripts/css.js
@@ -0,0 +1,5 @@
+var style = getComputedStyle(document.body);
+var primaryColor = style.getPropertyValue('--primary-color');
+var accentColor = style.getPropertyValue('--accent-color');
+var dangerColor = style.getPropertyValue('--red');
+var successColor = style.getPropertyValue('--success');
