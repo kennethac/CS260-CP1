68591588676ce5c703e50f8e6196d2f5e6624a91
Kenneth Christensen <1kennethac@gmail.com>
2019-02-08 18:31:58 -0700

    Should use api for existant classes.

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,76 +1,46 @@
-let totalProgress = {
-    datasets: [{
-        label: "Total Semester Progress",
-        data: [20, 80],
-        backgroundColor: [
-            successColor,
-            primaryColor
-        ]
-    }],
-    labels: [
-        "Complete",
-        "Incomplete"
-    ]
-};
-
-let englishProgress = {
-    datasets: [{
-        label: "English 316 Progress",
-        data: [45, 55],
-        backgroundColor: [
-            successColor,
-            primaryColor
-        ]
-    }],
-    labels: [
-        "Complete",
-        "Incomplete"
-    ]
-}
-
-let mathProgress = {
-    datasets: [{
-        label: "Math 313 Progress",
-        data: [12, 88],
-        backgroundColor: [
-            successColor,
-            primaryColor
-        ]
-    }],
-    labels: [
-        "Complete",
-        "Incomplete"
-    ]
-}
-
-let weekProgress = {
-    datasets: [{
-        label: "Progress this Week",
-        data: [96, 4],
-        backgroundColor: [
-            successColor,
-            primaryColor
-        ]
-    }],
-    labels: [
-        "Complete",
-        "Incomplete"
-    ]
-}
-
-let dataSets = {
-    'total': totalProgress,
-    'english': englishProgress,
-    'math': mathProgress,
-    'week': weekProgress
-}
-
-function initCharts() {
+async function initCharts() {
     let charts = document.querySelectorAll(".chart");
     for (var chart of charts) {
         let dataSet = chart.dataset.chart;
-        initChart(chart.getContext('2d'), dataSets[dataSet]);
+        initChart(chart.getContext('2d'), await getData(dataSet));
     }
+}
+
+async function getData(className) {
+    var result = await fetch("http://taskburner.kennethchristensen.me/api/tasks/" + className);
+    if (!result.ok) {
+        console.log("Error fetching information.")
+    }
+
+    var classData = await result.json();
+
+    var total = 0;
+    var complete = 0;
+    
+    for (var task of classData.tasks) {
+        if (task.complete) {
+            complete++;
+        }
+
+        total++;
+    }
+
+    var percent = complete/total;
+
+    return {
+        datasets: [{
+            label: classData.name + " Progress",
+            data: [percent, 1 - percent],
+            backgroundColor: [
+                successColor,
+                primaryColor
+            ]
+        }],
+        labels: [
+            "Complete",
+            "Incomplete"
+        ]
+    };
 }
 
 function initChart(context, dataset) {
